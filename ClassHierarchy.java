7cf670f5daf758456e180ce9f75a5f336c8c984a
deepthipaluri <dp6kf@mail.umkc.edu>
2013-03-12 22:25:00 -0500

    Create ClassHierarchy.java

---
+++ ClassHierarchy.java
@@ -0,0 +1,132 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.jena.ws;
+
+import com.hp.hpl.jena.ontology.*;
+import com.hp.hpl.jena.rdf.model.*;
+import com.hp.hpl.jena.shared.PrefixMapping;
+import com.hp.hpl.jena.util.iterator.Filter;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import java.io.PrintStream;
+import java.util.*;
+
+
+
+public class ClassHierarchy {
+    
+    protected OntModel m_model;
+    private Map m_anonIDs = new HashMap();
+    private int m_anonCount = 0;
+
+
+   
+    /** Show the sub-class hierarchy encoded by the given model */
+    public void showHierarchy( PrintStream out, OntModel m ) {
+        // create an iterator over the root classes that are not anonymous class expressions
+        Iterator i = m.listHierarchyRootClasses()
+                      .filterDrop( new Filter() {
+                                    public boolean accept( Object o ) {
+                                        return ((Resource) o).isAnon();
+                                    }} );
+
+        while (i.hasNext()) {
+            showClass( out, (OntClass) i.next(), new ArrayList(), 0 );
+        }
+    }
+
+
+   
+
+    /** Present a class, then recurse down to the sub-classes.
+     *  Use occurs check to prevent getting stuck in a loop
+     */
+    protected void showClass( PrintStream out, OntClass cls, List occurs, int depth ) {
+        renderClassDescription( out, cls, depth );
+        out.println();
+
+        // recurse to the next level down
+        if (cls.canAs( OntClass.class )  &&  !occurs.contains( cls )) {
+            for (Iterator i = cls.listSubClasses( true );  i.hasNext(); ) {
+                OntClass sub = (OntClass) i.next();
+
+                // we push this expression on the occurs list before we recurse
+                occurs.add( cls );
+                showClass( out, sub, occurs, depth + 1 );
+                occurs.remove( cls );
+            }
+        }
+    }
+
+
+    /**
+     * <p>Render a description of the given class to the given output stream.</p>
+     * @param out A print stream to write to
+     * @param c The class to render
+     */
+    public void renderClassDescription( PrintStream out, OntClass c, int depth ) {
+        indent( out, depth );
+
+        if (c.isRestriction()) {
+            renderRestriction( out, (Restriction) c.as( Restriction.class ) );
+        }
+        else {
+            if (!c.isAnon()) {
+                out.print( "Class " );
+                renderURI( out, c.getModel(), c.getURI() );
+                out.print( ' ' );
+            }
+            else {
+                renderAnonymous( out, c, "class" );
+            }
+        }
+    }
+
+    /**
+     * <p>Handle the case of rendering a restriction.</p>
+     * @param out The print stream to write to
+     * @param r The restriction to render
+     */
+    protected void renderRestriction( PrintStream out, Restriction r ) {
+        if (!r.isAnon()) {
+            out.print( "Restriction " );
+            renderURI( out, r.getModel(), r.getURI() );
+        }
+        else {
+            renderAnonymous( out, r, "restriction" );
+        }
+
+        out.print( " on property " );
+        renderURI( out, r.getModel(), r.getOnProperty().getURI() );
+    }
+
+    /** Render a URI */
+    protected void renderURI( PrintStream out, PrefixMapping prefixes, String uri ) {
+        out.print( prefixes.shortForm( uri ) );
+    }
+
+    /** Render an anonymous class or restriction */
+    protected void renderAnonymous( PrintStream out, Resource anon, String name ) {
+        String anonID = (String) m_anonIDs.get( anon.getId() );
+        if (anonID == null) {
+            anonID = "a-" + m_anonCount++;
+            m_anonIDs.put( anon.getId(), anonID );
+        }
+
+        out.print( "Anonymous ");
+        out.print( name );
+        out.print( " with ID " );
+        out.print( anonID );
+    }
+
+    /** Generate the indentation */
+    protected void indent( PrintStream out, int depth ) {
+        for (int i = 0;  i < depth; i++) {
+            out.print( "  " );
+        }
+    }
+}
+
